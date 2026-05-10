4520d6eb4febe91627c17cc9c08410932f01ba63
Sahil Bainya <sahilbainya2005@gmail.com>
2026-05-10 16:17:19 +0530

    add register controller

--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true, // optional
-}));
+  })
+);
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
